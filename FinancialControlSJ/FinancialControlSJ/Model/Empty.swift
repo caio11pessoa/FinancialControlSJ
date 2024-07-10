e3c059c4ae0453ab799f51e5cff6285a927676b8
CaioPessoa <caiopessoa1298@gmail.com>
2024-07-10 16:01:29 -0300

    setup architecture

---
+++ FinancialControlSJ/FinancialControlSJ/Model/Empty.swift
@@ -0,0 +1,8 @@
+//
+//  Empty.swift
+//  FinancialControlSJ
+//
+//  Created by Caio de Almeida Pessoa on 10/07/24.
+//
+
+import Foundation
---
+++ FinancialControlSJ/FinancialControlSJ/ViewModel/EmptyViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  EmptyViewModel.swift
+//  FinancialControlSJ
+//
+//  Created by Caio de Almeida Pessoa on 10/07/24.
+//
+
+import SwiftUI
+
+class EmptyViewModel: ObservableObject {
+    
+}
